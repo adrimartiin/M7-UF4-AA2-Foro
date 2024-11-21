3583eec6f5cfb118769b9f2ac8a2e6454d7d4832
adrimartin6 <adrianmartincalvet@gmail.com>
2024-11-21 16:55:07 +0100

    Archivo index.php actualizado, falta estilos y query para mostrar amigos

--- index.php
+++ index.php
@@ -1,13 +1,91 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <link href="./css/styles.css" rel="stylesheet">
+    <title>CodePlus</title>
+    <style>
+
+    </style>
 </head>
+
 <body>
-    <form action="./entrada/login.php">
-        <button>Login</button>
-    </form>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src="./img/nav_logo.png" id="nav_img"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Preguntas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Discusiones</a>
+                    </li>
+                </ul>
+                <form class="d-flex w-100" role="search">
+                    <input class="form-control search-bar" type="search" placeholder="Buscar" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit"><i
+                            class="fa-solid fa-magnifying-glass"></i></button>
+                </form>
+                <form action="./entrada/login.php" class="ms-3">
+                    <button class="btn btn-primary">Login</button>
+                </form>
+            </div>
+        </div>
+    </nav> <!-- fin de navbar -->
+
+    <div class="container">
+        <div class="barra-izquierda">
+            <h5><i class="fa-solid fa-users"></i> Users:</h5>
+            <!-- EN UN FUTURO SE GENERARÁN LOS USUARIOS -->
+            <p>Contenido 1</p>
+            <p>Contenido 2</p>
+            <p>Contenido 3</p>
+            <p>Contenido 4</p>
+            <p>Contenido 5</p>
+            <p>Contenido 6</p>
+            <p>Contenido 7</p>
+            <p>Contenido 8</p>
+            <p>Contenido 9</p>
+            <p>Contenido 10</p>
+            <p>Contenido 11</p>
+            <p>Contenido 12</p>
+            <p>Contenido 13</p>
+            <p>Contenido 14</p>
+            <p>Contenido 15</p>
+            <p>Contenido 16</p>
+            <p>Contenido 17</p>
+            <p>Contenido 18</p>
+            <p>Contenido 19</p>
+            <p>Contenido 20</p>
+            <p>Contenido 21</p>
+            <p>Contenido 22</p>
+            <p>Contenido 23</p>
+            <p>Contenido 24</p>
+            <p>Contenido 25</p>
+            <p>Contenido 26</p>
+            <p>Contenido 27</p>
+            <p>Contenido 28</p>
+            <p>Contenido 29</p>
+            <p>Contenido 30</p>
+        </div>
+        <div class="barra-derecha">
+            
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-HoA1K1fLdABEl+3t4zFQxtptCkQnz4BHo9LYUDe0w5l0yAyPi6gt74cHkXz6f1KP"
+            crossorigin="anonymous"></script>
 </body>
+
 </html>
