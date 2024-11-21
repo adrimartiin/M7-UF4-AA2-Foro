757e45ed98c6fd7c2a8b28adbe51fc3199c85f3d
kilianruiz <451093.joan23@fje.edu>
2024-11-21 15:26:23 +0100

    Formulario de Login Y register

---
+++ entrada/login.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+    <div class="login-container">
+        <h1>Iniciar Sesión</h1>
+        <form action="../validacionesPHP/validaLogin.php" method="POST">
+            <label for="username">Nombre de usuario</label>
+            <input type="text" name="username" placeholder="Nombre de usuario">
+            <label for="username">Contraseña</label>
+            <input type="password" name="password" placeholder="Contraseña">
+            <button type="submit" class="login-btn">Entrar</button>
+            <a href="./register.php">No tienes una cuenta? Registrarse</a>
+            <?php
+            if (isset($_GET['error'])) {
+                $error = $_GET['error'];
+                echo "<p style='color: red'>Usuario o contraseña incorrectos </p>";
+            }
+            ?>
+        </form>
+    </div>
+</body>
+</html>
---
+++ entrada/register.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+    <div class="login-container">
+        <h1>Resgístrate</h1>
+        <form action="../validaciones/validaRegister.php" method="POST">
+            <label for="nombre_usuario">Nombre de usuario</label>
+            <input type="text" name="nombre_usuario" placeholder="Nombre de usuario">
+            <label for="nombreReal">Nombre real</label>
+            <input type="text" name="nombreReal" placeholder="Nombre">
+            <label for="numTelefono">Número de teléfono</label>
+            <input type="tel" name="numTelefono" placeholder="Número de teléfono">
+            <label for="password">Contraseña</label>
+            <input type="password" name="password" placeholder="Contraseña">
+            <label for="repetirPassword">Repite la contraseña</label>
+            <input type="password" name="repetirPassword" placeholder="Repite la contraseña">
+            <button type="submit" class="login-btn">Entrar</button>
+            <?php
+                if(isset($_GET['error']) == '1'){
+                    echo "<p style='color: red'>El nombre de usuario ya existe</p>";
+                }
+            ?>
+        </form>
+    </div>
+</body>
+</html>
+
