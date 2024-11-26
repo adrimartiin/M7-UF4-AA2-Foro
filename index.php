fb44be0287cd9ad43f834d04a169ad3e0f0f593a
kilianruiz <451093.joan23@fje.edu>
2024-11-26 20:37:48 +0100

    variable de session del usuario y boton lo login y logout

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <!-- Font Awesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-    <!-- Custom Styles -->
     <link href="./css/styles.css" rel="stylesheet">
     <title>CodePlus</title>
 </head>
@@ -18,27 +18,33 @@
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
             <a class="navbar-brand" href="#"><img src="./img/nav_logo.png" id="nav_img" alt="Logo"></a>
+            <?php 
+                 if(!isset($_SESSION['usuario'])){
+                    // Botón que abre el popup de login 
+                    ?>
+                    <form action="./entrada/login.php">
+                        <?php
+                    echo '<button class="btn btn-primary ms-3">Login</button>';
+                    ?>
+                    </form>
+                    <?php
+                 } else {
+                    echo $_SESSION['usuario'];
+                 }
+                 ?>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Preguntas</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Discusiones</a>
-                    </li>
-                </ul>
                 <form class="d-flex w-100" role="search">
                     <input class="form-control search-bar" type="search" placeholder="Buscar" aria-label="Search">
                     <button class="btn btn-outline-success" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
                 </form>
-                <form action="./entrada/login.php" class="ms-3">
-                    <button class="btn btn-primary">Login</button>
-                </form>
             </div>
+            <form action = "./paginas/cerrar_sesion.php" method = "POST">
+            <input type="submit" value="Logout" class="btn btn-primary ms-3" name="logout">
+            </form>
         </div>
     </nav>
 
@@ -52,7 +58,7 @@
                 <i class="fa-solid fa-comments me-2"></i><span>Discusiones</span>
             </a>
             <br>
-            <a href="#" name="preguntas" class="d-flex align-items-center text-decoration-none">
+            <a href="./paginas/preguntas.php" name="preguntas" class="d-flex align-items-center text-decoration-none">
                 <i class="fa-solid fa-question-circle me-2"></i><span>Preguntas</span>
             </a>
             <br>
@@ -77,9 +83,6 @@
         </div>
     </div>
 
-    <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-HoA1K1fLdABEl+3t4zFQxtptCkQnz4BHo9LYUDe0w5l0yAyPi6gt74cHkXz6f1KP" crossorigin="anonymous"></script>
 </body>
 
 </html>
