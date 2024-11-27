b8a269af519600db94f63d5625295f05de88693e
adrimartin6 <adrianmartincalvet@gmail.com>
2024-11-27 11:35:29 +0100

    Modificacion BBDD

--- BBDD/bd_foro.sql
+++ BBDD/bd_foro.sql
@@ -1,33 +1,39 @@
--- SQLBook: Code
 CREATE DATABASE bd_foro;
 
 USE bd_foro;
 
-CREATE TABLE tbl_usuarios(
+-- Crear tabla de usuarios
+CREATE TABLE tbl_usuarios (
     id_usuario INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    nombre_usuario VARCHAR(50) ,
-    nombreReal_usuario VARCHAR(70) ,
+    nombre_usuario VARCHAR(50),
+    nombreReal_usuario VARCHAR(70),
     telf_usuario CHAR(9),
     psswd_usuario VARCHAR(60)
 );
 
-create TABLE tbl_preguntas( 
+-- Crear tabla de preguntas
+CREATE TABLE tbl_preguntas( 
     id_preguntas INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     texto_preguntas VARCHAR(255) NOT NULL,
     titulo_preguntas VARCHAR(150) NOT NULL,
     estado_preguntas ENUM ('no respondida', 'guardada', 'respondida') NOT NULL DEFAULT 'no respondida',
-    FOREIGN KEY (id_usuarios) REFERENCES tbl_usuarios(tbl_usuarios)
+    id_usuario INT NOT NULL,
+    FOREIGN KEY (id_usuario) REFERENCES tbl_usuarios(id_usuario)
 );
 
-create TABLE tbl_respuestas(
+-- Crear tabla de respuestas
+CREATE TABLE tbl_respuestas(
     id_respuestas INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     texto_respuestas VARCHAR(255) NOT NULL,
     titulo_respuestas VARCHAR(150) NOT NULL,
-    estado_respuestas ENUM('no conestada', 'contestada') NOT NULL DEFAULT 'no contestada';
+    estado_respuestas ENUM('no contestada', 'contestada') NOT NULL DEFAULT 'no contestada',
+    id_preguntas INT NOT NULL,
+    id_usuario INT NOT NULL,
     FOREIGN KEY (id_preguntas) REFERENCES tbl_preguntas(id_preguntas),
-    FOREIGN KEY (id_usuarios) REFERENCES tbl_usuarios(id_usuarios)
+    FOREIGN KEY (id_usuario) REFERENCES tbl_usuarios(id_usuario)
 );
 
+-- Insertar datos en la tabla de usuarios
 INSERT INTO tbl_usuarios (nombre_usuario, nombreReal_usuario, telf_usuario, psswd_usuario) 
 VALUES 
 ('Juan123', 'Juan Pérez', '123456789', 'qweQWE123'),
